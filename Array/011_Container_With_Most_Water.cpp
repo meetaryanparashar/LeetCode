74746fa0d65c40e0b953c00381cbba6ea4653b2a
Aryan Parashar <meetaryanparashar@gmail.com>
2025-10-06 22:50:14 +0530

    added problem 011

---
+++ Array/011_Container_With_Most_Water.cpp
@@ -0,0 +1,44 @@
+/*
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// My first optimal approach.
+int maxArea(vector<int> &height)
+{
+    int l = 0, r = height.size()-1;
+    int maxWater = INT16_MIN;
+    while (l < r)
+    {
+        int waterArea = min(height[l], height[r]) * (r - l);
+        if (waterArea > maxWater)
+        {
+            maxWater = waterArea;
+        }
+        if (height[l] < height[r])
+        {
+            l++;
+        }
+        else
+        {
+            r--;
+        }
+    }
+    return maxWater;
+}
+
+int main()
+{
+    vector<int> input = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    cout << maxArea(input);
+    return 0;
+}
