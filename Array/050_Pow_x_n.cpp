3de0ec7e7366523b704d4ec21136963a91a7d070
Aryan Parashar <meetaryanparashar@gmail.com>
2025-10-07 22:57:05 +0530

    added problem 050

---
+++ Array/050_Pow_x_n.cpp
@@ -0,0 +1,96 @@
+/*
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+Constraints:
+
+-100.0 < x < 100.0
+-231 <= n <= 231-1
+n is an integer.
+Either x is not zero or n > 0.
+-104 <= xn <= 104
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// My brute force approach. - Time limit exceeded.
+double myPowBF(double x, int n)
+{
+    double ans = 1;
+    while (n > 0)
+    {
+        ans = ans * x;
+        n--;
+    }
+    while (n < 0)
+    {
+        ans = ans / x;
+        n++;
+    }
+    return ans;
+}
+
+// My Second approach. ----------- After Spending Hours I finally got this accepted by my own.
+double myPow(double x, int n)
+{
+    double ans = 1;
+    int raise = 1;
+    double raise_pow = x;
+    bool Invert = false;
+    bool minCase = false;
+    if (n < 0)
+    {
+        if (n == INT32_MIN)
+        {
+            cout << "yare yare daza"<<endl;
+            n = INT32_MIN + 1;
+            cout << n<<endl;
+            minCase = true;
+        }
+
+        n = n * -1;
+        Invert = true;
+    }
+
+    while (n > 0)
+    {
+        if (raise <= n / 2 && raise <= INT32_MAX / 2)
+        {
+            // cout<< "Raise = "<<raise<<" Raise Power = "<<raise_pow<<endl;
+            raise_pow = raise_pow * raise_pow;
+            raise = raise * 2;
+        }
+        else
+        {
+            // cout << "raise "<<raise << " is greater than n = "<<n<<endl;
+            ans = ans * raise_pow;
+            n = n - raise;
+            raise = 1;
+            raise_pow = x;
+        }
+    }
+
+    if (Invert)
+    {
+        if (minCase)
+        {
+            ans = ans * x;
+        }
+        return (1 / ans);
+    }
+    else
+    {
+        return ans;
+    }
+}
+
+int main()
+{
+    // cout << myPow(1, 2147483647);
+    cout << myPow(2, -2147483648);
+    // cout << myPow(2, -3);
+    // cout << myPow(2.0, 10);
+    // cout << INT32_MIN;
+    return 0;
+}
