5484fd7df20ddbe2e5f10c3d8de092575f20e532
Aryan Parashar <meetaryanparashar@gmail.com>
2025-10-18 22:56:10 +0530

    added P 852

---
+++ Array/852_Peak_Index_in_a_Mountain_Array.cpp
@@ -0,0 +1,35 @@
+/*
+You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
+
+Return the index of the peak element.
+
+Your task is to solve it in O(log(n)) time complexity.
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int peakIndexInMountainArray(vector<int>& arr) {
+        int l = 1, r = arr.size()-2;  // assuming peak is not at start or end of array.
+
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if (arr[mid-1] < arr[mid] && arr[mid + 1] < arr[mid]){
+                return mid;
+            }
+            if (arr[mid-1] < arr[mid]){
+                l = mid+1;
+            }
+            else{
+                r = mid-1;
+            }
+        }
+    }
+
+int main(){
+    vector<int> input = {3,5,3,2,0};
+    cout << peakIndexInMountainArray(input);
+    return 0;
+}
