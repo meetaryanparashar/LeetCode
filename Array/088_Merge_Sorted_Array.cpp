2304972d63ddc53436984d5731e9f1cb1850cca9
Aryan Parashar <meetaryanparashar@gmail.com>
2025-11-04 20:50:40 +0530

    better approach for P88

--- Array/088_Merge_Sorted_Array.cpp
+++ Array/088_Merge_Sorted_Array.cpp
@@ -10,36 +10,72 @@ The final sorted array should not be returned by the function, but instead be st
 #include <vector>
 using namespace std;
 
-void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for (int i = 0; i < n; i++)
+// first approach O(m*n)
+void mergeFA(vector<int> &nums1, int m, vector<int> &nums2, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int prev = m + i - 1;
+        while (prev >= 0 && nums1[prev] > nums2[i])
+        // so if first condition of && is false then it will not even check the other condition
         {
-            int prev = m+i-1;
-            while (prev >=0 &&nums1[prev] > nums2[i] ) 
-            // so if first condition of && is false then it will not even check the other condition
-            {
-                nums1[prev+1] = nums1[prev];
-                prev--;
-            }
-            nums1[prev+1] = nums2[i];
+            nums1[prev + 1] = nums1[prev];
+            prev--;
         }
+        nums1[prev + 1] = nums2[i];
     }
+}
 
-int main(){
+// second approach O(m+n)
+void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+{
+    int idx = m+n-1;
+    int p1 = m-1, p2 = n-1;
+    
+    while (p1 >= 0 && p2 >= 0){
+        if (nums1[p1] > nums2[p2])
+        {
+            nums1[idx] = nums1[p1];
+            p1--;
+        }
+        else{
+            nums1[idx] = nums2[p2];
+            p2--;
+        }
+        idx--;
+    }
+    while (p1 >= 0)
+    {
+        nums1[idx] = nums1[p1];
+        p1--;
+        idx--;
+    }
+    while (p2 >= 0)
+    {
+        nums1[idx] = nums2[p2];
+        p2--;
+        idx--;
+    }
+    
+}
+
+int main()
+{
     vector<int> input1 = {0};
     vector<int> input2 = {1};
 
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < 1; i++)
     {
         cout << input1[i] << " ";
     }
 
     cout << endl;
-    
+
     merge(input1, 0, input2, 1);
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < 1; i++)
     {
         cout << input1[i] << " ";
     }
-    
+
     return 0;
 }
