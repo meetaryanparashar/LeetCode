8c370d2e58e0e53d93af9b0eda85c976dfa8b96f
Aryan Parashar <meetaryanparashar@gmail.com>
2026-01-09 19:55:44 +0530

    Better approach for P1

--- Array/001_Two_Sum.cpp
+++ Array/001_Two_Sum.cpp
@@ -7,6 +7,7 @@ You can return the answer in any order.
 */
 
 #include <iostream>
+#include <algorithm>
 #include <vector>
 
 using namespace std;
@@ -31,10 +32,34 @@ vector<int> twoSum(vector<int> &nums, int target)
     return ans;
 }
 
+// sort method. (from apna college) But changes indices
+vector<int> twoSumS(vector<int> &nums, int target)
+{
+    vector<int> ans(2, 0);
+    int l = 0;
+    int r = nums.size() -1;
+
+    sort(nums.begin(), nums.end());
+
+    while(l < r){
+        int sum = nums[l] + nums[r];
+        if (sum == target)
+        {
+            ans[0] = l;
+            ans[1] = r;
+            return ans;
+        }
+        else if(sum > target){r--;}
+        else{l++;}
+        
+    }
+    return ans;
+}
+
 int main()
 {
     vector<int> input = {13, 11, 7, 23, 2};
-    vector<int> ans =  twoSum(input, 9);
+    vector<int> ans =  twoSumS(input, 9);
     cout << ans[0]<<", "<<ans[1];
 
     return 0;
